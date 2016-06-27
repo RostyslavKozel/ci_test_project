390efa498eaeb0111e66839672437ea3820b59dc
rostyslav_kozel <lonepiligrim@list.ru>
2016-06-27 17:24:40 +0300

    update bd

--- bd/127.0.0.1 .sql
+++ bd/127.0.0.1 .sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Хост: 127.0.0.1
--- Время создания: Июн 22 2016 г., 11:03
+-- Время создания: Июн 27 2016 г., 17:23
 -- Версия сервера: 5.5.25
 -- Версия PHP: 5.3.13
 
@@ -42,8 +42,10 @@ CREATE TABLE IF NOT EXISTS `ci_sessions` (
 --
 
 INSERT INTO `ci_sessions` (`session_id`, `ip_address`, `user_agent`, `last_activity`, `user_data`) VALUES
-('1a948e15a9dff2359396f667fba8f895', '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36', 1466580468, ''),
-('a59cf0e2c187136f7c1f8950f142ef17', '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36', 1466582400, '');
+('7a8da17e4dd53f424709f31e30309250', '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36', 1467035374, 'a:6:{s:9:"user_data";s:0:"";s:8:"username";s:5:"Anton";s:6:"userId";s:1:"3";s:12:"userTimeZone";s:7:"2:00GTM";s:19:"user_remind_changes";s:1:"1";s:9:"logged_in";b:1;}'),
+('7f1b9e0cbc3630f712da78149d3971a9', '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36', 1467035047, ''),
+('9845affdd7eb645c7d4c2b826fa80faa', '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36', 1467035044, ''),
+('ed708523f20d561dfd1e1abf88b39f15', '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36', 1467033651, '');
 
 -- --------------------------------------------------------
 
@@ -56,19 +58,20 @@ CREATE TABLE IF NOT EXISTS `users` (
   `name` varchar(40) NOT NULL,
   `pwd` varchar(50) NOT NULL,
   `time_zone` text NOT NULL,
+  `remind_changes` tinyint(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=8 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
 
 --
 -- Дамп данных таблицы `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `pwd`, `time_zone`) VALUES
-(1, 'user1', '123qwe', '2:00GTM'),
-(2, 'user2', '123qwe', '-5:00GTM'),
-(3, 'Anton', '123qwe', '3:00GTM'),
-(4, 'user3', '123qwe', '1:00GTM'),
-(5, 'user3', '123qwe', '3:00GTM');
+INSERT INTO `users` (`id`, `name`, `pwd`, `time_zone`, `remind_changes`) VALUES
+(1, 'user1', '123qwe', '1:00GTM', 0),
+(2, 'user2', '123qwe', '2:00GTM', 0),
+(3, 'Anton', '123qwe', '2:00GTM', 1),
+(4, 'user3', '123qwe', '3:00GTM', 0),
+(5, 'user3', '123qwe', '3:00GTM', 0);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
